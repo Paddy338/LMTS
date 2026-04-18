7106b8b9b419dce787a26baa9a27d8c575c4457c
Paddy338 <13675133772@139.com>
2026-04-18 13:06:15 +0800

    Update build.ps1 with sha1 algorithm, timestamp and full python command

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 # 在项目根目录运行
-pyinstaller -F -w .\send.pyw --icon .\icons\sendicon.ico
-pyinstaller -F -w .\receive.pyw --icon .\icons\receiveicon.ico
+python -m PyInstaller -F -w .\send.pyw --icon .\icons\sendicon.ico
+python -m PyInstaller -F -w .\receive.pyw --icon .\icons\receiveicon.ico
 
 
 $ExeFiles=@(
@@ -14,7 +14,7 @@ $PvkPath = "D:\Develop\LMTS_second_dev\cert\root.pvk"
 foreach($exe in $ExeFiles){
     if (Test-Path $exe){
         Write-Host "正在为 $exe 添加数字签名..."
-        & "D:\Green_Software\cert\signcode.exe" -spc $Spcpath -v $Pvkpath $exe
+        & "D:\Green_Software\cert\signcode.exe" -spc $Spcpath -v $Pvkpath -a sha1 -t http://timestamp.digicert.com $exe
         if($LASTEXITCODE -eq 0){
             Write-Host "√ 签名成功"
         }
